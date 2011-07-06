005532133f12f012e79d4eee423a9459b5c6633c
Justin <npcomple@naver.com>
2011-07-06 21:50:24 +0900

    nested attributs

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,13 +1,8 @@
 class Ride < ActiveRecord::Base
-  has_many :tracks
-  has_many :ride_monitors
+  has_many :tracks, :dependent => :destroy
+  has_many :ride_monitors, :dependent => :destroy
 
   accepts_nested_attributes_for :tracks, :allow_destroy => true
   accepts_nested_attributes_for :ride_monitors, :allow_destroy => true
-  
-  # def ride_monitor_attributes=(ride_monitor_attributes)
-    # ride_monitor_attributes.each do |attributes|
-      # ride_monitors.build(attributes)
-    # end
-  # end
+
 end
