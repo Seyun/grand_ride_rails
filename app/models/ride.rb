0d32ed46382b5f7a2a3323fd931dd8f13debb403
Justin <npcomple@naver.com>
2011-07-06 17:04:57 +0900

    added

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -2,9 +2,12 @@ class Ride < ActiveRecord::Base
   has_many :tracks,         :dependent => :destroy
   has_many :ride_monitors,  :dependent => :destroy
 
-  def ride_monitor_attributes=(ride_monitor_attributes)
-    ride_monitor_attributes.each do |attributes|
-      ride_monitors.build(attributes)
-    end
-  end
+  accepts_nested_attributes_for :tracks, :allow_destroy => true
+  accepts_nested_attributes_for :ride_monitors, :allow_destroy => true
+  
+  # def ride_monitor_attributes=(ride_monitor_attributes)
+    # ride_monitor_attributes.each do |attributes|
+      # ride_monitors.build(attributes)
+    # end
+  # end
 end
