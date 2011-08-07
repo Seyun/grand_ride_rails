eec701e36d3f940bfb75973e7625daf5f8db882b
Justin <npcomple@naver.com>
2011-08-07 15:32:36 +0900

    .

--- config/routes.rb
+++ config/routes.rb
@@ -61,7 +61,7 @@ GrandRideRails::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index_old.html.
-  root :to => "rides#index"
+  root :to => "rides#new"
 
   # See how all your routes lay out with "rake routes"
 
